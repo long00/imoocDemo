0b19b7fce762cae0bfcf6c483bac4a771877dba8
yangtenglong <1573702011@qq.com>
2020-02-24 21:07:09 +0800

    修改部分注释

--- src/main/java/com/big/king/service/impl/ProductCategoryServiceImpl.java
+++ src/main/java/com/big/king/service/impl/ProductCategoryServiceImpl.java
@@ -1,13 +1,3 @@
-/**
- * Copyright (C), 2015-2020, XXX有限公司
- * FileName: ProductCategoryServiceImpl
- * Author:   king
- * Date:     2020/02/08 22:13
- * Description:
- * History:
- * <author>          <time>          <version>          <desc>
- * 作者姓名           修改时间           版本号              描述
- */
 package com.big.king.service.impl;
 
 import com.big.king.mapper.ProductCategoryMapper;
